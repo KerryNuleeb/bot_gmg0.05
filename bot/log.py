f979e27b1f997e4c96eb2733ff13e98a5df4ced6
Kerry <vladislav150199@mail.ru>
2020-02-09 15:34:26 +0300

    0.05

---
+++ bot/log.py
@@ -0,0 +1,6 @@
+def get_token():
+    return 'baa27c049c62ddd53dff29f2a44be8cd35ffcd464bce7913902b1f9568ca1de42b7892b20a82abc5e7e2c'
+def get_login():
+    return '+375296256040'
+def get_password():
+    return 'kek150199kek'
